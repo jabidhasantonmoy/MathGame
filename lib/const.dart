ef0cd1329506e7b912f1100c18f68443460a5934
jabidhasantonmoy <jabidhasan1998@gmail.com>
2022-07-28 20:49:57 +0600

    Expanded added with dummy text

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var whiteTextStyle = const TextStyle(
+  color: Colors.black,
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'const.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,32 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[300],
-      body: Column(),
+      body: Column(
+        children: [
+          //level progress
+          Container(
+            height: 160,
+            color: Colors.deepPurple,
+          ),
+          Expanded(
+            child: Container(
+              //color: const Color(0xff8d3bd1),
+              child: Center(
+                child: Text(
+                  '1 + 1 = ?',
+                  style: whiteTextStyle,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: const Color(0xff62fc86),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
